9e7c07d7106cdbe5129223c8bfd572f497ce02bb
Mouad0006 <jamalfahal6@gmail.com>
2025-04-18 01:24:07 +0100

    Update auth.php

--- auth.php
+++ auth.php
@@ -2,7 +2,6 @@
 
 header("Content-Type: application/json");
 
-// تحقق من التوكن
 $validToken = "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"; 
 
 if (!isset($_GET['token']) || $_GET['token'] !== $validToken) {
